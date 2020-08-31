a4787be90d90f90c9772bbf45ed002288aaafb0b
Júlia W <w.julia91@gmail.com>
2020-08-31 12:54:55 +0200

    action log created w style

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: '🧔🏻 hits 👾 for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -20,7 +27,12 @@ new Vue({
         },
 
         specialAttack: function () {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: '🧔🏻 hits 👾 hard for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -34,17 +46,26 @@ new Vue({
                 this.
                 playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: '🧔🏻 heals for 10'
+            });
             this.monsterAttacks();
         },
 
         giveUp: function () {
             this.gameIsRunning = false;
-            
+
         },
 
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: '👾 hits 🧔🏻 for  ' + damage
+            });
         },
 
         calculateDamage: function (min, max) {
